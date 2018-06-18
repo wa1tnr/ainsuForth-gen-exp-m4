04c5fff61b4f7c7556f487a56b6ce6546da229c9
wa1tnr <wa1tnr@users.noreply.github.com>
2018-06-18 23:24:01 +0000

    modified:   ainsuForth-gen-exp-m4.ino

--- ainsuForth-gen-exp-m4.ino
+++ ainsuForth-gen-exp-m4.ino
@@ -1,7 +1,9 @@
 // Mon 18 Jun 21:39:23 UTC 2018
 // 4737-a3a-0f5-
 
-// Mon 18 Jun 23:21:55 UTC 2018
+// On branch testing-bb
+
+// Mon 18 Jun 23:23:37 UTC 2018
 // +crickit_cpx_as_itsy_m0
 
 // crickit CPX supported when compiled as ItsyBitsy M0.
