fe18f5b7e5245475b100eaf810ab724a12e61bb1
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-28 18:36:00 +0000

    New method to update the end user through the user interface.
    On branch testing-ff-
    	modified:   ainsuForthsketch.h

--- ainsuForthsketch.h
+++ ainsuForthsketch.h
@@ -1,10 +1,20 @@
-// Mon Jan 15 18:14:33 UTC 2018
-// 4737-a0d-05c-
+// Tue Aug 28 18:10:58 UTC 2018
+// 4737-a3c-00c- // ainsuForthsketch.h
+// On branch testing-ff-
 
-// version bump
+// previous:
+// Tue Aug 28 18:10:58 UTC 2018 // 4737-a3c-00b- // On branch testing-ee
+// Mon Jan 15 18:14:33 UTC 2018 // 4737-a0d-05c-
+// Tue Jun 20 21:33:06 UTC 2017 // 4735-a0p-02-
 
-// Tue Jun 20 21:33:06 UTC 2017
-// 4735-a0p-02-
+#define timeStamp(t)     "Tue Aug 28 18:10:58 UTC 2018"
+#define shredStamp(s)    "4737-a3c-00c-"
+#define progNameStamp(p) "ainsuForth-gen-exp-m4"
+#define targetStamp(t)   "ItsyBitsy M4 Express"
+#define branchStamp(b)   "testing-ff-"
+#define featureList(f)   "+fm4x +crickitCPX"
+#define dateStamp(e)     "28 Aug 2018" // infrequently maintained, in the past
+#define gitShowStamp(g)  "4c47e5506e9" // arbitrary length to fit aesthetics
 
 #include "src/kernel/signon.h"
 #include "src/kernel/getline.h"
