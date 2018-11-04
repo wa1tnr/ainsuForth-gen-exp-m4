3e825319262d6f3446d5e36d9210a5905db358bb
wa1tnr <wa1tnr@users.noreply.github.com>
2018-11-04 02:43:39 +0000

    Final commit for the evening.  Hopefully.
    On branch testing-gg-
    
    	modified:   ainsuForthsketch.h

--- ainsuForthsketch.h
+++ ainsuForthsketch.h
@@ -1,3 +1,7 @@
+// Sun Nov  4 02:32:24 UTC 2018
+// 4737-a3c-00e- // ainsuForth-gen-exp-m4
+// On branch testing-gg-
+
 // Sun Nov  4 01:50:28 UTC 2018
 // 4737-a3c-00d- // ainsuForth-gen-exp-m4
 // On branch testing-gg-
@@ -11,14 +15,14 @@
 // Mon Jan 15 18:14:33 UTC 2018 // 4737-a0d-05c-
 // Tue Jun 20 21:33:06 UTC 2017 // 4735-a0p-02-
 
-#define timeStamp(t)     "Sun Nov  4 01:50:28 UTC 2018"
+#define timeStamp(t)     "Sun Nov 4 02:32:24 2018"
 #define shredStamp(s)    "4737-a3c-00d-"
 #define progNameStamp(p) "ainsuForth-gen-exp-m4"
 #define targetStamp(t)   "Trellis M4 Express"
 #define branchStamp(b)   "testing-gg-      "
 #define featureList(f)   "+trm4x +fm4x +crickitCPX"
 #define dateStamp(e)     "04 Nov 2018" // infrequently maintained, in the past
-#define gitShowStamp(g)  "134166be3a9" // arbitrary length to fit aesthetics
+#define gitShowStamp(g)  "8c8141d2866" // arbitrary length to fit aesthetics
 
 #include "src/kernel/signon.h"
 #include "src/kernel/getline.h"
