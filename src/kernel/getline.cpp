79cd3b8c19994b08e49d15b77d3695896065b516
wa1tnr <wa1tnr@users.noreply.github.com>
2018-11-10 23:47:56 +0000

    New series - reusing branch testing-hh-
    getline.cpp and friends - condtional compilation
    for multiple targets with multiple capabilities.
    
    On branch testing-hh-
    	modified:   getline.cpp

--- src/kernel/getline.cpp
+++ src/kernel/getline.cpp
@@ -1,7 +1,15 @@
 // Sun 17 Jun 22:09:15 UTC 2018
 // 4737-a3a-0e0-
-
+char junk;
 // TODO: move some of this stuff elsewhere - way beyond the scope of the getline word.
+#ifndef DEF_GETLINE_CPP_ONCE
+    #define DEF_GETLINE_CPP_ONCE
+#include "getline_options.h"
+
+// #warning HERE_WE_ARE in getline.cpp
+
+#ifdef NEVER_DEFINED_TEN_FOUR // nonsense tag to prevent compile
+#warning NEVER_DEFINED_TEN_FOUR defined in getline.cpp
 
 // development of directory and file creation, so
 // that the system may operate in a self-contained way.
@@ -903,4 +911,6 @@ void ascii_xfer_spi_flash_main(void) {
   // delay(100);
 // }
 
+#endif // #ifdef NEVER_DEFINED_TEN_FOUR - line 6
+#endif // #ifndef DEF_GETLINE_CPP_ONCE
 // END.
