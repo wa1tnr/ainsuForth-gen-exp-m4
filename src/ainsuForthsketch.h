619d49fb309f766587b47d528014b1e5a860639c
wa1tnr <wa1tnr@users.noreply.github.com>
2022-02-16 22:12:24 +0000

    All indications: program runs fine.
    The trimming of that one long offensive file
    seems to be going well - no changes in its
    behavior have been noted.
    
    	modified:   ainsuForthsketch.h
    
    On branch dvlp-aa

--- src/ainsuForthsketch.h
+++ src/ainsuForthsketch.h
@@ -4,16 +4,14 @@
 //     Sun Nov  4 02:32:24 UTC 2018
 //     4737-a3c-00e- // ainsuForth-gen-exp-m4
 
-#define timeStamp(t)     "Wed Feb 16 20:36:53 2022"
-#define shredStamp(s)    "xxxx-a3c-00n-"
+#define timeStamp(t)     "Wed Feb 16 21:44:43 2022"
+#define shredStamp(s)    "xxxx-a3c-00p-"
 #define progNameStamp(p) "ainsuForth-gen-exp-m4"
-#define targetStamp(t)   "NeoTrellis M4 ~413~ "
-
-#define branchStamp(b)   "n-dvlp-aa-DEBUG-a          "
-
+#define targetStamp(t)   "NeoTrellis M4 ~417~ "
+#define branchStamp(b)   "dvlp-aa                    "
 #define featureList(f)   "+SPIFl2k19 +trm4x +fm4x +crickitCPX"
 #define dateStamp(e)     "16 Feb 2022"
-#define gitShowStamp(g)  "a62a14eef78"
+#define gitShowStamp(g)  "6bdb15820de"
 
 #include "kernel/signon.h"
 #include "kernel/getline.h"
